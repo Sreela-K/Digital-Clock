003079153ba95ae4d52809281cf6fa4519dba944
Sreela-k <k2sreela@gmail.com>
2024-01-14 12:44:34 +0530

    final project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        manga: '#0f3443',
+        nanga: '#34e89e',
+        thenga: '#FFEBD8',
+        kolla: '#F05941'
+      }
+    },
+  },
+  plugins: [],
+}
+
+
